8bae84ccaab0a6bad25fdd30ddd40daec8c402ee
Clay Tobolka <tobolkac@gmail.com>
2020-09-16 20:31:52 -0500

    clean up and multi video support

--- edX Super Speed.user.js
+++ edX Super Speed.user.js
@@ -9,50 +9,32 @@
 // @run-at document-idle
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    var vid = document.getElementsByClassName("video")[0];
-    var vid_url = vid.attributes['data-metadata'].value.toString().match("https://edx-video.net/[\\w\-]+.mp4")[0];
-    var vid_title = vid.previousElementSibling.innerText;
-    var zNode = document.createElement ('div');
-    zNode.innerHTML = '<button id="myButton" type="button"><a href=' + vid_url + '>Download ' + vid_title + ' lecture video</a></button>';
-    zNode.setAttribute ('id', 'myContainer');
-    zNode.style = "top:0;right:0;position:absolute;z-index:99999;padding:20px;";
-    document.body.appendChild (zNode);
+'use strict';
+var vid = document.getElementsByClassName("video")[0];
+var vid_url = vid.attributes['data-metadata'].value.toString().match("https:\/\/edx-video.net\/.*\.mp4")[0];
+var vid_title = vid.previousElementSibling.innerText;
+var zNode = document.createElement ('div');
+zNode.innerHTML = '<button id="myButton" type="button"><a href=' + vid_url + '>Download ' + vid_title + ' lecture video</a></button>';
+zNode.setAttribute ('id', 'myContainer');
+zNode.style = "top:0;right:0;position:absolute;z-index:99999;padding:20px;";
+document.body.appendChild (zNode);
 
-    var item = document.getElementsByClassName("video-speeds")[0];
+var i;
+for (i = 0; i < document.getElementsByClassName("video-speeds").length; i++) {
+    addSpeed("2.0", i)
+    addSpeed("2.5", i)
+    addSpeed("3.0", i)
+}
+
+function addSpeed(speed, videoIndex) {
+    var item = document.getElementsByClassName("video-speeds")[videoIndex];
     var new_speed = document.createElement("li");
     var btn = document.createElement("button");
-    var speed_limit = "2.0";
     btn.setAttribute("class", "control speed-option");
     btn.setAttribute("tabindex", -1);
     btn.setAttribute("aria-pressed", "false");
     new_speed.appendChild(btn);
-    new_speed.setAttribute("data-speed", speed_limit);
-    new_speed.children[0].innerText = speed_limit + "x";
+    new_speed.setAttribute("data-speed", speed);
+    new_speed.children[0].innerText = speed + "x";
     item.prepend(new_speed);
-
-    item = document.getElementsByClassName("video-speeds")[0];
-    new_speed = document.createElement("li");
-    btn = document.createElement("button");
-    speed_limit = "2.5";
-    btn.setAttribute("class", "control speed-option");
-    btn.setAttribute("tabindex", -1);
-    btn.setAttribute("aria-pressed", "false");
-    new_speed.appendChild(btn);
-    new_speed.setAttribute("data-speed", speed_limit);
-    new_speed.children[0].innerText = speed_limit + "x";
-    item.prepend(new_speed);
-
-    item = document.getElementsByClassName("video-speeds")[0];
-    new_speed = document.createElement("li");
-    btn = document.createElement("button");
-    speed_limit = "3.0";
-    btn.setAttribute("class", "control speed-option");
-    btn.setAttribute("tabindex", -1);
-    btn.setAttribute("aria-pressed", "false");
-    new_speed.appendChild(btn);
-    new_speed.setAttribute("data-speed", speed_limit);
-    new_speed.children[0].innerText = speed_limit + "x";
-    item.prepend(new_speed);
-})();
+}
